6e09e94240ae9f089a9243d4a55f728e8ebc775b
realllllyx18 <71406802+realllllyx18@users.noreply.github.com>
2020-11-03 12:17:54 -0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -12,3 +12,43 @@ The program should display the factored form in an Entry widget.
 Extension: make the + between a,b and b,c buttons that will toggle
 between + and -.
 """
+
+import tkinter as tk
+from tkinter import * 
+import math
+
+window = tk.Tk()
+
+eoutput = StringVar()
+eoutput.set("Output part")
+
+def clickFunction():
+    num1 = e1.get()
+    b = float(num1)
+    num2 = e1.get()
+    c = float(num2)
+    answer = (-b+math.sqrt(b*2-4*c))/2 and (-b-math.sqrt(b*2-4*c))/2
+    a_entry.delete(0,END)
+    a_entry.insert(0,answer)
+
+l1 = Label(window, text = "Enter the value of b")
+e1 = Entry(window, width = 20)
+l2 = Label(window, text = "Enter the value of c")
+e2 = Entry(window, width = 20)
+b1 = Button(window, text = "Click to calculate", command = clickFunction)
+a_label = Label(window, text = "Answer goes here: ")
+a_entry = Entry(window, width = 20, textvariable = eoutput)
+
+l1.grid(row=1,column=1)
+e1.grid(row=1,column=1)
+l2.grid(row=1,column=1)
+e2.grid(row=1,column=1)
+b1.grid(row=1,column=1)
+a_label.grid(row=2,column=1)
+a_entry.grid(row=3,column=2)
+
+window.mainloop
+
+
+
+
